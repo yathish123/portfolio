1e3ea2f8552f93fc785f1501df0b3326bd1e13fd
yathish123 <59306051+yathish123@users.noreply.github.com>
2024-04-04 21:25:17 +0530

    Add files via upload

---
+++ internship/script.js
@@ -0,0 +1,45 @@
+alert("Welcome to my portfolio")
+function changeAboutMeText() 
+{
+    const aboutMeTexts = ["java developer", "cloud developer", "Full Stack Web Developer"]; // Add more texts as needed
+    const typingSpeed = 100; // milliseconds per character
+    const eraseSpeed = 50; // milliseconds per character during erasing
+    const pauseTime = 1500; // milliseconds to pause between each text change
+    const aboutMeElement = document.querySelector('.about-me');
+
+    let textIndex = 0;
+    let charIndex = 0;
+    let isDeleting = false;
+
+    function type() 
+    {
+        const currentText = aboutMeTexts[textIndex];
+        /* Typing */
+        if (!isDeleting && charIndex < currentText.length) 
+        {
+            aboutMeElement.textContent += currentText[charIndex];
+            charIndex++;
+            setTimeout(type, typingSpeed);
+        }
+        /* Erasing */
+        else if (isDeleting && charIndex > 0) {
+            aboutMeElement.textContent = currentText.substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(type, eraseSpeed);
+        } 
+        /* Switching the deleting or Typing process */
+        else {
+            isDeleting = !isDeleting;
+            if (!isDeleting) {
+                textIndex = (textIndex + 1) % aboutMeTexts.length;
+            }
+            setTimeout(type, pauseTime);
+        }
+    }
+
+    type();
+}
+
+// Call function to add stunning modification
+changeAboutMeText();
+
